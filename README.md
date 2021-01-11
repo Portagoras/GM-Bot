54534118fa546227caa340fab712427fa22d0c99
Christian Weber <christian.m.weber.94@gmail.com>
2021-01-12 00:25:06 +0100

    Literally the first commit...

---
+++ README.md
@@ -0,0 +1,3 @@
+# GM Bot
+
+Literally just a bot that answers to "gm" (not casesensitive) with a random adjective + noun. Nothign more, nothing less...
---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js')
+const client = new Discord.Client();
+const fs = require('fs');
+
+const G = fs.readFileSync('g.dat', 'utf8').split('\r\n');
+const M = fs.readFileSync('m.dat', 'utf8').split('\r\n');
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
+  }
+
+generateResponse = function() {
+    return G.random().capitalize() + " " + M.random().capitalize() + "!";
+}
+
+client.on('message', msg =>{
+    if(msg.content.toLowerCase() === 'gm')
+        msg.reply(generateResponse());
+});
+console.log(generateResponse());
+console.log(generateResponse());
+console.log(generateResponse());
+console.log(generateResponse());
+console.log(generateResponse());
+console.log(generateResponse());
+
+client.login('')
